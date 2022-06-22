94f8a5d9f7d34d04ca375e7b17d601d6ba645315
Aaryan Dewan <aaryandewan@yahoo.com>
2022-06-22 22:42:02 +0530

    update login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 
-function login() {
+export default function Login() {
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  useEffect(() => {
+    document.title = "Login-Instagram";
+  }, []);
+
   return <div>login page</div>;
 }
 
-export default login;
+// export default login;
